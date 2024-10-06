80db0e8bc41ba0cf9e38118f16947b8c9584bc0e
ahmetkcak <ahmet.aliberk07@gmail.com>
2024-10-06 11:52:45 +0300

    Add project files.

---
+++ CaseDemo/Controllers/RolesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseDemo.Controllers
+{
+    public class RolesController(RoleManager<IdentityRole> roleManager) : Controller
+    {
+        public IActionResult Index()
+        {
+            var roles= roleManager.Roles;
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(IdentityRole role)
+        {
+            if(!roleManager.RoleExistsAsync(role.Name).GetAwaiter().GetResult())
+            {
+                roleManager.CreateAsync(new IdentityRole(role.Name)).GetAwaiter().GetResult();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CaseDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CaseDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
